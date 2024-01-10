9383e7fdf46ac6ca93131bddbfd8744ee01cd9af
bira10a <94951002+bira10a@users.noreply.github.com>
2024-01-10 16:44:15 +0300

    added files scss

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <header className='header'>
+                <div className='container'>
+                    <a className='header__link' href='#'>asdsadasd</a>
+                </div>
+            </header>
+        </>
+    )
+};
+
+export default Header;
