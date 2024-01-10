e7a23d8e68a22bef53b081e025753ec928b5db61
bira10a <94951002+bira10a@users.noreply.github.com>
2024-01-10 17:09:04 +0300

    added img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import Logo from '../../img/logo.svg';
+
 const Header = () => {
     return (
         <>
             <header className='header'>
-                <div className='container'>
-                    <a className='header__link' href='#'>asdsadasd</a>
+                <div className='header__container'>
+                    <a href='#'>
+                        <img src={Logo} loading='lazy' width='110' height='110' alt='logo'></img>
+                    </a>
+
+                    <ul className='header__list'>
+                        <li className='header__item'>
+                            <a href='#'>Главная</a>
+                        </li>
+                        <li className='header__item'>
+                            <a href='#'>О нас</a>
+                        </li>
+                        <li className='header__item'>
+                            <a href='#'>Места</a>
+                        </li>
+                        <li className='header__item'>
+                            <a href='#'>Контакты</a>
+                        </li>
+                        <li className='header__item'>
+                            <a href='#'>Баня</a>
+                        </li>
+                    </ul>
                 </div>
             </header>
         </>
